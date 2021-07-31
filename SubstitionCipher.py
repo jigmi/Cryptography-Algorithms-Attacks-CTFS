359270676cd3b85b65e39cfc350b8c433770db99
jigmi <87882680+jigmi@users.noreply.github.com>
2021-07-31 17:42:55 +1000

    Update SubstitionCipher.py

--- SubstitionCipher.py
+++ SubstitionCipher.py
@@ -1,21 +1,37 @@
+import random
 def key():
-  key = {}
-  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  for letters in alphabet:
-    generator = random.choice(alphabet)
-    key[letters] = generator
-    alphabet = alphabet.replace(generator,"")
-   return key
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = {}
+    for letters in alphabet:
+        generator = random.choice(alphabet)
+        key[letters] = generator
+        alphabet = alphabet.replace(generator,"")
+    return key
 def encrypt(key,message):
-  ciphertext = ""
-  for letters in message:
-    if letters in key:
-      ciphertext += key[letters]
-     else:
-      ciphertext += letters
-  return ciphertext
+    message = message.upper()
+    ciphertext = ""
+    for letters in message:
+        if letters in key:
+            ciphertext += key[letters]
+        else:
+            ciphertext += letters
+    return ciphertext
 def decrypt(ciphertext):
-  
+    decrypted_message = ""
+    for letters in ciphertext:
+        for values in alphabet:
+            if letters in key[values]:              
+                decrypted_message += values
+            else:
+                pass
+    return decrypted_message
+key = key()
+print(key)
+ciphertext = encrypt(key,"I love pikachu but i also like charizard")
+plain_text = decrypt(ciphertext).lower()
+print(plain_text)
+#Attack commencing below by Eve. 
+
   
       
    
